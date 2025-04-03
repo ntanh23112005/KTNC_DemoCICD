10aca891aa195e48f6c6f5558bef923abd098890
ntanh23112005 <anhntps38560@gmail.com>
2025-04-03 14:45:14 +0700

    First commit

---
+++ src/main/java/org/cicd/Caculator.java
@@ -0,0 +1,11 @@
+package org.cicd;
+
+public class Caculator {
+    public Integer sum (Integer a, Integer b){
+        return 0;
+    }
+
+    public Integer sub(Integer a, Integer b){
+        return 0;
+    }
+}
